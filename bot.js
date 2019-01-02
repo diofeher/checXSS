5694249c29d1eaa5c130a917742c68aac399a736
Diógenes Augusto Fernandes Hermínio <diofeher@gmail.com>
2019-01-01 22:58:27 -0300

    bot motherfucker

---
+++ bot.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+function sleep(millis) {
+  return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+function pwn(payload) {
+  void (async (payload) => {
+    const browser = await puppeteer.launch({headless: false, args:['--disable-xss-auditor']});
+    const page = await browser.newPage();
+    page.on('dialog', (dialog) => {
+      console.log('Payload OK!', payload);
+      dialog.accept();
+    });
+    // const that = this;
+    await page.goto('http://localhost:8000/');
+    // await page.setCookie(...cookies);
+    await page.type('input[name=test]', payload);
+    await sleep(1000);
+    await page.click('input[name=submit]');
+    await page.waitForNavigation();
+    try {
+      await sleep(1000);
+    } catch (err) {
+      console.error('err', err);
+    }
+    await browser.close();
+  })(payload);
+}
+
+fs.readFile('pay_sml.txt', 'utf-8', function(err, contents) {
+  for(var payload of contents.split(/\r\n|\r|\n/)) {
+    pwn(payload);
+  };
+});
+
+  
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php echo $_POST['test'] ?>
+
+<form action="." method="POST" />
+    <input type="text" name="test"/>
+    <input type="submit" name="submit" value="Send.">
+</form>
